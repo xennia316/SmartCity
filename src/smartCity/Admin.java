8a1ebdea4c4bd1975a3a66cbe57440ba6ae5eb8c
xennia316 <xennia316@gmail.com>
2022-05-12 21:09:08 +0100

    admin methods modified

--- src/smartCity/Admin.java
+++ src/smartCity/Admin.java
@@ -14,11 +14,12 @@ public class Admin extends User {
         String town = read.nextLine();
 
         System.out.print("Enter new hotel price: ");
-        int evaluation = read.nextInt();
+        int price = read.nextInt();
 
         System.out.print("Enter new hotel evaluation: ");
-        double price = read.nextInt();
-        read.close();
+        int evaluation = read.nextInt();
+
+       // read.close();
 
         return new Hotel(name, price, town, evaluation);
 
@@ -27,7 +28,6 @@ public class Admin extends User {
     // subroutine
     public static void adminSubroutine(ArrayList<Hotel> hotels) {
         Scanner read = new Scanner(System.in);
-
         System.out.print("1. To Register a new hotel\n2. To go to user mode\n");
 
         int newChoice = read.nextInt();
@@ -42,12 +42,12 @@ public class Admin extends User {
                 n -= 1;
             } while (n > 0);
 
-            read.close();
+            //read.close();
         }
 
         else if (newChoice == 2) {
             System.out.print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-            System.out.print("WELCOME TRUSTED USER\n");
+            System.out.print("YOU'RE NOW IN USER MODE\n");
             System.out.print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
             User.userSubroutine();
         }
